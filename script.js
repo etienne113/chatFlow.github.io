1b31943187a4a18839b0797966a96a7022149079
Etienne <96786848+etienne113@users.noreply.github.com>
2023-09-16 17:12:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,19 +15,6 @@ const createChatLi = (message, className) => {
     chatLi.querySelector("p").textContent = message;
     return chatLi; // return chat <li> element
 }
-// Create a function to load CSS files
-function loadCSS(url) {
-    const linkElement = document.createElement("link");
-    linkElement.rel = "stylesheet";
-    linkElement.href = url;
-    document.head.appendChild(linkElement);
-}
-
-// Load the CSS files
-loadCSS("https://etienne113.github.io/chatFlow.github.io/style.css");
-loadCSS("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0");
-loadCSS("https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0");
-
 
 const generateResponse = (chatElement) => {
     const API_URL = "http://127.0.0.1:5000/upload";
