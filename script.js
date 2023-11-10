9e9fb6562be3eb8a81ba51c1644c0b1a0f2d5ed5
Etienne <96786848+etienne113@users.noreply.github.com>
2023-11-10 15:58:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,14 +11,36 @@ const createChatLi = (message, className) => {
     // Create a chat <li> element with passed message and className
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", `${className}`);
-    chatLi.innerHTML = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatLi.innerHTML = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">psychology</span><p></p>`;
     chatLi.querySelector("p").textContent = message;
     return chatLi; // return chat <li> element
+}
+
+const createLoader = () => {
+    const loader = document.createElement("div");
+    loader.classList.add("loader");
+    return loader;
+};
+
+let selectedOption;
+function updateSelectedOption(){
+    const options = document.getElementsByName('options');
+    for (let i = 0; i < options.length; i++) {
+        if (options[i].checked) {
+            selectedOption = options[i].id;
+            break;
+        }
+    }
 }
 
 const generateResponse = (chatElement) => {
     const API_URL = "http://127.0.0.1:5000/get-answer";
     const messageElement = chatElement.querySelector("p");
+
+    // Display the loader while waiting for the response
+    const loader = createLoader();
+    chatbox.appendChild(loader);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
 
     // Define the properties and message for the API request
     const requestOptions = {
@@ -28,22 +50,31 @@ const generateResponse = (chatElement) => {
         },
         body: JSON.stringify({
             body: userMessage,
+            department: selectedOption,
             mode: 'cors'
         })
-    }
+    };
 
-    // Send POST request to API, get response and set the reponse as paragraph text
-    fetch(API_URL, requestOptions).then(res => res.json()).then(r => {
-        messageElement.textContent = r.answer.trim()
-    }).catch(() => {
-        messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Something went wrong. Please try again.";
-    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-}
+    // Send POST request to API, get response and set the response as paragraph text
+    fetch(API_URL, requestOptions)
+        .then(res => res.json())
+        .then(r => {
+            messageElement.textContent = r.answer.trim();
+        })
+        .catch(() => {
+            messageElement.classList.add("error");
+            messageElement.textContent = "Oops! Something went wrong. Please try again.";
+        })
+        .finally(() => {
+            // Remove the loader after receiving the response
+            chatbox.removeChild(loader);
+            chatbox.scrollTo(0, chatbox.scrollHeight);
+        });
+};
 
 const handleChat = () => {
-    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
-    if(!userMessage) return;
+    userMessage = chatInput.value.trim();
+    if (!userMessage) return;
 
     // Clear the input textarea and set its height to default
     chatInput.value = "";
@@ -58,9 +89,10 @@ const handleChat = () => {
         const incomingChatLi = createChatLi("Thinking...", "incoming");
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
+        updateSelectedOption();
         generateResponse(incomingChatLi);
     }, 600);
-}
+};
 
 chatInput.addEventListener("input", () => {
     // Adjust the height of the input textarea based on its content
@@ -79,4 +111,10 @@ chatInput.addEventListener("keydown", (e) => {
 
 sendChatBtn.addEventListener("click", handleChat);
 closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
-chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+chatbotToggler.addEventListener("click", () => {
+    document.body.classList.toggle("show-chatbot");
+
+    // Additional animation logic for the toggler
+    const rotateDeg = document.body.classList.contains("show-chatbot") ? 90 : 0;
+    chatbotToggler.style.transform = `rotate(${rotateDeg}deg)`;
+});
